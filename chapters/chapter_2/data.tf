4e3132fcad807f1819fefecc8fdd4b0bc2383759
Pedro Oliveira <pedro_hro1992@outlook.com>
2023-10-17 18:18:21 -0300

    reconfigure

--- chapters/chapter_2/data.tf
+++ chapters/chapter_2/data.tf
@@ -1,7 +1,7 @@
 data "aws_vpc" "default" {
   default = true
 }
-
+d
 
 data "aws_subnets" "default" {
     filter {
--- chapters/chapter_7/eks-cluster/main.tf
+++ chapters/chapter_7/eks-cluster/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "eks_cluster" {
-  source = "/home/poliveira/Workspace/personal/terraform-modules/services/eks-cluster"
+  source = "/home/pedro/Workspace/personal/projects/terraform-modules/services/eks-cluster"
 
   name         = "example-eks-cluster"
   min_size     = 1
@@ -13,35 +13,35 @@ module "eks_cluster" {
   instance_types = ["t3.small"]
 }
 
-provider "kubernetes" {
-  host                   = module.eks_cluster.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
+# provider "kubernetes" {
+#   host                   = module.eks_cluster.cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+# }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks_cluster.cluster_name
-}
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = module.eks_cluster.cluster_name
+# }
 
-module "simple_webapp" {
-  source = "/home/poliveira/Workspace/personal/terraform-modules/services/k8s-app"
+# module "simple_webapp" {
+#   source = "/home/poliveira/Workspace/personal/terraform-modules/services/k8s-app"
 
-  name           = "simple-webapp"
-  image          = "training/webapp"
-  replicas       = 2
-  containet_port = 5000
+#   name           = "simple-webapp"
+#   image          = "training/webapp"
+#   replicas       = 2
+#   containet_port = 5000
 
-  environment_variables = {
-    PROVIDER = "terraform"
-  }
+#   environment_variables = {
+#     PROVIDER = "terraform"
+#   }
 
-  create_lb_eks = true
+#   create_lb_eks = true
 
-  #Only deploy the app after the cluster has been deployed
-  depends_on = [module.eks_cluster]
-}
+#   #Only deploy the app after the cluster has been deployed
+#   depends_on = [module.eks_cluster]
+# }
 
-output "service_endpoint" {
-  value       = module.simple_webapp.service_endpoint
-  description = "The K8s Endpoint"
-}
+# output "service_endpoint" {
+#   value       = module.simple_webapp.service_endpoint
+#   description = "The K8s Endpoint"
+# }
