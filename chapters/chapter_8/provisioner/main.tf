3c4d618cfa4ab29ed4a12fca31e691c1ffb06a6b
Pedro Oliveira <pedro_hro1992@outlook.com>
2023-10-17 19:23:47 -0300

    End of Chapter 8

---
+++ chapters/chapter_8/provisioner/main.tf
@@ -0,0 +1,72 @@
+provider "aws" {
+  region = "us-east-2"
+}
+data "aws_ami" "ubuntu" {
+
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+}
+
+
+resource "aws_security_group" "instance" {
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  public_key = tls_private_key.example.public_key_openssh
+}
+//Resource Instance with local exec
+# resource "aws_instance" "example" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = "t2.micro"
+
+#   provisioner "local-exec" {
+#     command = "echo \"Hello, Workd from $(uname -smp)\""
+#   }
+# }
+
+
+resource "aws_instance" "example" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  key_name               = aws_key_pair.generated_key.key_name
+
+  provisioner "remote-exec" {
+    inline = ["echo \"Hello, Workd from $(uname -smp)\""]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = tls_private_key.example.private_key_pem
+  }
+}
+
+resource "null_resource" "example" {
+  # Use UUID to force this null_resource to be recreated on every cakk to 'terraform apply'
+  triggers = {
+    uuid = uuid()
+  }
+  provisioner "local-exec" {
+    command = "echo \"Hello, Workd from $(uname -smp)\""
+  }
+}
